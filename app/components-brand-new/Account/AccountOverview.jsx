1b9a2392c9475279798e904e5a6167568cbea720
igorbiliba <igorbiliba@mail.ru>
2018-09-01 19:15:26 +0300

    +

--- app/components-brand-new/Account/AccountOverview.jsx
+++ app/components-brand-new/Account/AccountOverview.jsx
@@ -37,6 +37,10 @@ import AccountTreemap from "../../components/Account/AccountTreemap";
 import {getBackedCoin} from "common/gatewayUtils";
 import AssetWrapper from "../../components/Utility/AssetWrapper";
 
+import ReserveAssetModal from "../../components/Modal/ReserveAssetModal";
+import BaseModal from "../../components/Modal/BaseModal";
+import ZfApi from "react-foundation-apps/src/utils/foundation-api";
+
 class AccountOverview extends React.Component {
     constructor(props) {
         super();
@@ -73,6 +77,21 @@ class AccountOverview extends React.Component {
         }
 
         this._handleFilterInput = this._handleFilterInput.bind(this);
+    }
+
+    _reserveButtonClick(assetId, e) {
+        e.preventDefault();
+        this.setState({reserve: assetId});
+        ZfApi.publish("reserve_asset", "open");
+    }
+
+    _reserveAsset(account_id, e) {
+        e.preventDefault();
+        ZfApi.publish("reserve_asset", "close");
+        let {issue} = this.state;
+        let asset = this.props.assets.get(issue.asset_id);
+        issue.amount *= utils.get_asset_precision(asset.precision);
+        AssetActions.issueAsset(account_id, issue);
     }
 
     _handleFilterInput(e) {
@@ -581,6 +600,29 @@ class AccountOverview extends React.Component {
                         ) : (
                             emptyCell
                         )}
+                    </td>
+                    <td
+                        style={{textAlign: "center"}}
+                        className="column-hide-small"
+                        data-place="bottom"
+                        data-tip={counterpart.translate("tooltip.burn")}
+                    >
+                        <a
+                            style={{marginRight: 0}}
+                            className={
+                                includeAsset ? "order-cancel" : "action-plus"
+                            }
+                            onClick={this._reserveButtonClick.bind(
+                                this,
+                                asset.id
+                            )}
+                        >
+                        <span class="icon fire icon-14px">
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                <path d="M10.031 32c-2.133-4.438-0.997-6.981 0.642-9.376 1.795-2.624 2.258-5.221 2.258-5.221s1.411 1.834 0.847 4.703c2.493-2.775 2.963-7.196 2.587-8.889 5.635 3.938 8.043 12.464 4.798 18.783 17.262-9.767 4.294-24.38 2.036-26.027 0.753 1.646 0.895 4.433-0.625 5.785-2.573-9.759-8.937-11.759-8.937-11.759 0.753 5.033-2.728 10.536-6.084 14.648-0.118-2.007-0.243-3.392-1.298-5.312-0.237 3.646-3.023 6.617-3.777 10.27-1.022 4.946 0.765 8.568 7.555 12.394z"></path>
+                            </svg>
+                        </span>
+                        </a>
                     </td>
                     <td
                         style={{textAlign: "center"}}
@@ -1356,7 +1398,21 @@ class AccountOverview extends React.Component {
             }
         ];
 
-        return <Tabs items={items} inner={true} />;
+        return <div>
+            <Tabs items={items} inner={true} />
+            <BaseModal id="reserve_asset" overlay={true}>
+                <br />
+                <div className="grid-block vertical">
+                    <ReserveAssetModal
+                        assetId={this.state.reserve}
+                        account={account}
+                        onClose={() => {
+                            ZfApi.publish("reserve_asset", "close");
+                        }}
+                    />
+                </div>
+            </BaseModal>
+            </div>;
     }
 }
 
--- app/components/Icon/Icon.jsx
+++ app/components/Icon/Icon.jsx
@@ -17,6 +17,7 @@ let icons = [
     "connected",
     "connect",
     "cross-circle",
+    "fire",
     "dashboard",
     "deposit",
     "disconnected",
