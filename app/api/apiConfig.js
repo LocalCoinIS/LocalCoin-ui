53937e6ea5e738e88ab8d8d1ec655bf8a58c6c51
Your Name <you@example.com>
2018-10-17 19:20:58 +0300

    +

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -67,14 +67,14 @@ export const gdexAPIs = {
 
 export const settingsAPIs = {
     // DEFAULT_WS_NODE: "wss://fake.automatic-selection.com",
-    DEFAULT_WS_NODE: "ws://194.63.142.61:8090",
+    DEFAULT_WS_NODE: "wss://moscow.localcoin.is/",
     WS_NODE_LIST: [
         {
             url: "wss://fake.automatic-selection.com",
             location: {translate: "settings.api_closest"}
         },
         {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
-        {url: "ws://194.63.142.61:8090", location: "Gateis staging"}
+        {url: "wss://moscow.localcoin.is/", location: "Gateis staging"}
     ],
     DEFAULT_FAUCET: "http://194.63.142.61:5000", // 2017-12-infrastructure worker proposal
     TESTNET_FAUCET: "https://faucet.testnet.bitshares.eu",
