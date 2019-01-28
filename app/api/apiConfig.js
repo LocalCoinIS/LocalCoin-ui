0699ffbdd573bf0ef4d315432aa8ab0998b8c509
Manao <manao@manao.by>
2019-01-28 15:55:42 +0300

    fix market test node

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -1,69 +1,69 @@
-// export const blockTradesAPIs = {
-//     BASE: "https://api.blocktrades.us/v2",
-//     COINS_LIST: "/coins",
-//     ACTIVE_WALLETS: "/active-wallets",
-//     TRADING_PAIRS: "/trading-pairs",
-//     DEPOSIT_LIMIT: "/deposit-limits",
-//     ESTIMATE_OUTPUT: "/estimate-output-amount",
-//     ESTIMATE_INPUT: "/estimate-input-amount"
-// };
-//
-// export const openledgerAPIs = {
-//     BASE: "https://ol-api1.openledger.info/api/v0/ol/support",
-//     COINS_LIST: "/coins",
-//     ACTIVE_WALLETS: "/active-wallets",
-//     TRADING_PAIRS: "/trading-pairs",
-//     DEPOSIT_LIMIT: "/deposit-limits",
-//     ESTIMATE_OUTPUT: "/estimate-output-amount",
-//     ESTIMATE_INPUT: "/estimate-input-amount"
-// };
-//
-// export const rudexAPIs = {
-//     BASE: "https://gateway.rudex.org/api/v0_1",
-//     COINS_LIST: "/coins",
-//     NEW_DEPOSIT_ADDRESS: "/new-deposit-address"
-// };
-//
-// export const cryptoBridgeAPIs = {
-//     BASE: "https://api.crypto-bridge.org/api/v1",
-//     COINS_LIST: "/coins",
-//     ACTIVE_WALLETS: "/wallets",
-//     MARKETS: "/markets",
-//     TRADING_PAIRS: "/trading-pairs"
-// };
-//
-// export const widechainAPIs = {
-//     BASE: "https://gateway.winex.pro/api/v0/ol/support",
-//     COINS_LIST: "/coins",
-//     ACTIVE_WALLETS: "/active-wallets",
-//     NEW_DEPOSIT_ADDRESS: "/new-deposit-address",
-//     WITHDRAW_HISTORY: "/latelyWithdraw",
-//     TRADING_PAIRS: "/trading-pairs",
-//     DEPOSIT_HISTORY: "/latelyRecharge"
-// };
-//
-// export const gdex2APIs = {
-//     BASE: "https://api.gdex.io/adjust",
-//     COINS_LIST: "/coins",
-//     ACTIVE_WALLETS: "/active-wallets",
-//     TRADING_PAIRS: "/trading-pairs"
-// };
-//
-// // Legacy Deposit/Withdraw
-// export const gdexAPIs = {
-//     BASE: "https://api.gdex.io",
-//     ASSET_LIST: "/gateway/asset/assetList",
-//     ASSET_DETAIL: "/gateway/asset/assetDetail",
-//     GET_DEPOSIT_ADDRESS: "/gateway/address/getAddress",
-//     CHECK_WITHDRAY_ADDRESS: "/gateway/address/checkAddress",
-//     DEPOSIT_RECORD_LIST: "/gateway/deposit/recordList",
-//     DEPOSIT_RECORD_DETAIL: "/gateway/deposit/recordDetail",
-//     WITHDRAW_RECORD_LIST: "/gateway/withdraw/recordList",
-//     WITHDRAW_RECORD_DETAIL: "/gateway/withdraw/recordDetail",
-//     GET_USER_INFO: "/gateway/user/getUserInfo",
-//     USER_AGREEMENT: "/gateway/user/isAgree",
-//     WITHDRAW_RULE: "/gateway/withdraw/rule"
-// };
+export const blockTradesAPIs = {
+    BASE: "https://api.blocktrades.us/v2",
+    COINS_LIST: "/coins",
+    ACTIVE_WALLETS: "/active-wallets",
+    TRADING_PAIRS: "/trading-pairs",
+    DEPOSIT_LIMIT: "/deposit-limits",
+    ESTIMATE_OUTPUT: "/estimate-output-amount",
+    ESTIMATE_INPUT: "/estimate-input-amount"
+};
+
+export const openledgerAPIs = {
+    BASE: "https://ol-api1.openledger.info/api/v0/ol/support",
+    COINS_LIST: "/coins",
+    ACTIVE_WALLETS: "/active-wallets",
+    TRADING_PAIRS: "/trading-pairs",
+    DEPOSIT_LIMIT: "/deposit-limits",
+    ESTIMATE_OUTPUT: "/estimate-output-amount",
+    ESTIMATE_INPUT: "/estimate-input-amount"
+};
+
+export const rudexAPIs = {
+    BASE: "https://gateway.rudex.org/api/v0_1",
+    COINS_LIST: "/coins",
+    NEW_DEPOSIT_ADDRESS: "/new-deposit-address"
+};
+
+export const cryptoBridgeAPIs = {
+    BASE: "https://api.crypto-bridge.org/api/v1",
+    COINS_LIST: "/coins",
+    ACTIVE_WALLETS: "/wallets",
+    MARKETS: "/markets",
+    TRADING_PAIRS: "/trading-pairs"
+};
+
+export const widechainAPIs = {
+    BASE: "https://gateway.winex.pro/api/v0/ol/support",
+    COINS_LIST: "/coins",
+    ACTIVE_WALLETS: "/active-wallets",
+    NEW_DEPOSIT_ADDRESS: "/new-deposit-address",
+    WITHDRAW_HISTORY: "/latelyWithdraw",
+    TRADING_PAIRS: "/trading-pairs",
+    DEPOSIT_HISTORY: "/latelyRecharge"
+};
+
+export const gdex2APIs = {
+    BASE: "https://api.gdex.io/adjust",
+    COINS_LIST: "/coins",
+    ACTIVE_WALLETS: "/active-wallets",
+    TRADING_PAIRS: "/trading-pairs"
+};
+
+// Legacy Deposit/Withdraw
+export const gdexAPIs = {
+    BASE: "https://api.gdex.io",
+    ASSET_LIST: "/gateway/asset/assetList",
+    ASSET_DETAIL: "/gateway/asset/assetDetail",
+    GET_DEPOSIT_ADDRESS: "/gateway/address/getAddress",
+    CHECK_WITHDRAY_ADDRESS: "/gateway/address/checkAddress",
+    DEPOSIT_RECORD_LIST: "/gateway/deposit/recordList",
+    DEPOSIT_RECORD_DETAIL: "/gateway/deposit/recordDetail",
+    WITHDRAW_RECORD_LIST: "/gateway/withdraw/recordList",
+    WITHDRAW_RECORD_DETAIL: "/gateway/withdraw/recordDetail",
+    GET_USER_INFO: "/gateway/user/getUserInfo",
+    USER_AGREEMENT: "/gateway/user/isAgree",
+    WITHDRAW_RULE: "/gateway/withdraw/rule"
+};
 
 export const settingsAPIs = {
     // DEFAULT_WS_NODE: "wss://fake.automatic-selection.com",
