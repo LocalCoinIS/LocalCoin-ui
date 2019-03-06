f3f249226c03ac26d925735c82edaee0d9552e65
Manao <manao@manao.by>
2019-03-06 17:25:00 +0300

    fix liquid only checkbox

--- app/components-brand-new/Exchange/MyMarkets.jsx
+++ app/components-brand-new/Exchange/MyMarkets.jsx
@@ -1370,7 +1370,7 @@ export default connect(
                 starredMarkets: SettingsStore.getState().starredMarkets,
                 onlyLiquid: SettingsStore.getState().viewSettings.get(
                     "onlyLiquid",
-                    true
+                    false
                 ),
                 defaultMarkets: SettingsStore.getState().defaultMarkets,
                 viewSettings: SettingsStore.getState().viewSettings,
