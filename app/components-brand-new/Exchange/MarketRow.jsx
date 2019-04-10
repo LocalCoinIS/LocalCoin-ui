4766edf367199e04b96c65fb9b8e5323cce0f30f
Manao <manao@manao.by>
2019-04-10 12:58:39 +0300

    fix trade pairs selection

--- app/components-brand-new/Exchange/MarketRow.jsx
+++ app/components-brand-new/Exchange/MarketRow.jsx
@@ -363,7 +363,9 @@ class MarketRow extends React.Component {
         if (this.props.currentMarket === marketID) {
             className += " activeMarket";
         }
-
+        console.log(111);
+        console.log(this.props.currentMarket);
+        console.log(111);
         return (
             <tr className={className} style={rowStyles}>
                 {columns}
