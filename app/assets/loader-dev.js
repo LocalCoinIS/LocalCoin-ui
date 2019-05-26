ff27c867402dd41862f4ae00db6135ea8137dde1
Nineseven <nine@nineseven.ru>
2019-05-26 10:46:59 +0300

    Asset new-brand merged

--- app/assets/loader-dev.js
+++ app/assets/loader-dev.js
@@ -1,8 +1,8 @@
-require("./brand-new-layout/sass/main.sass");
+require("./sass/main.sass");
 require("./stylesheets/app.scss");
 //require("./brand-new-layout/css/fonts.css");
 //require("./brand-new-layout/css/custom.css");
-require("./brand-new-layout/img/images");
+require("./img/images");
 require("file-loader?name=index.html!./index-dev.html");
 require("file-loader?name=favicon.ico!./favicon.ico");
 // require("file-loader?name=dictionary.json!common/dictionary_en.json");
