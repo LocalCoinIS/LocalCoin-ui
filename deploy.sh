0ea423f1bbea55a3495be3c2851aed30466999d9
Nineseven <nine@nineseven.ru>
2019-06-06 17:51:47 +0300

    Travis builder test

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 gitkeys # this key should have push access
 ssh-add gitkeys
 git add --all
-git commit -m "Travis deploy $TRAVIS_BUILD_NUMBER"
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER updating $TRAVIS_BRANCH [skip ci]"
 git remote set-url origin git@github.com:LocalCoinIS/LocalCoin-ui.git
 git push origin $TRAVIS_BRANCH
