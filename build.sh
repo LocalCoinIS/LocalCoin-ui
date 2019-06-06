1c6a33a3bd3d555a4f9509aedb98ca6596658c5d
Nineseven <nine@nineseven.ru>
2019-06-06 17:11:02 +0300

    +Deploy.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 cd $TRAVIS_BUILD_DIR
 unamestr=`uname`
+echo TRAVIS_BRANCH=$TRAVIS_BRANCH
 
 if [ "$unamestr" == 'Linux' ]
 then
+    git checkout $TRAVIS_BRANCH
     npm run build
 fi
 ## Build the binaries if this is a release tag
