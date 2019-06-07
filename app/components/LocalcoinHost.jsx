7e9cf241ad06a1c63837976db9d0d4e1ef0ba164
igor biliba <igorbiliba@mail.ru>
2019-06-07 13:26:06 +0300

    ADD: replay+read log+info ui

--- app/components/LocalcoinHost.jsx
+++ app/components/LocalcoinHost.jsx
@@ -1,7 +1,7 @@
 export default class LocalcoinHost {
 	_LOCALCOIN_HOST_URL = null;
 	LOCALCOIN_HOST_URL = () => {
-		return "http://localhost:52209";//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		//return "http://localhost:52209";
 		if(this._LOCALCOIN_HOST_URL !== null) return this._LOCALCOIN_HOST_URL;
 
 		try {
