d664beef4a3ea70062e6cf7b05dcd5320aa55151
Nineseven <nine@nineseven.ru>
2019-08-01 15:00:59 +0300

    Update nodes list

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -1,5 +1,5 @@
 export const settingsAPIs = {
-    DEFAULT_WS_NODE: "wss://fi.localcoin.is",
+    DEFAULT_WS_NODE: "wss://de1.localcoin.is",
     WS_NODE_LIST: [
         {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
         {url: "wss://mynodes.de", location: "US NY by ip24"},
@@ -11,8 +11,6 @@ export const settingsAPIs = {
         {url: "wss://mysupremecoin.com", location: "Italy by the-supreme19"},
         {url: "wss://bebe3101.space", location: "Germany by bebe3101"},
         {url: "wss://au.localcoin.is", location: "Australia by ip23"},
-        {url: "wss://de.localcoin.is", location: "Germany"},
-        {url: "wss://fi.localcoin.is", location: "Finland"},
         {url: "wss://ru.localcoin.is", location: "Russia"}
     ],
     DEFAULT_FAUCET: "https://faucet.localcoin.is", // 2018-12-infrastructure worker proposal
