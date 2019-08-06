966471bc4e83ffc20109bee302f345c679672cfe
Fluke <42545106+LocalCoinIS@users.noreply.github.com>
2019-08-06 12:51:45 +0100

    Update apiConfig.js

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -2,15 +2,12 @@ export const settingsAPIs = {
     DEFAULT_WS_NODE: "wss://de1.localcoin.is",
     WS_NODE_LIST: [
         {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
-        {url: "wss://mynodes.de", location: "US NY by ip24"},
         {url: "wss://us.localcoin.is", location: "US Denver by crazy-indain"},
         {url: "wss://ru2.localcoin.is", location: "Russia by albert-andersen"},
         {url: "wss://id.localcoin.is", location: "Indonesia by billpresta3101"},
-        {url: "wss://ua.localcoin.is", location: "Ukraine by l0l"},
         {url: "wss://de1.localcoin.is", location: "Germany by firedswiss1"},
         {url: "wss://mysupremecoin.com", location: "Italy by the-supreme19"},
         {url: "wss://bebe3101.space", location: "Germany by bebe3101"},
-        {url: "wss://au.localcoin.is", location: "Australia by ip23"},
         {url: "wss://ru.localcoin.is", location: "Russia"}
     ],
     DEFAULT_FAUCET: "https://faucet.localcoin.is", // 2018-12-infrastructure worker proposal
