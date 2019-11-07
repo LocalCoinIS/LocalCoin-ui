d25f8174fd7e3aff691f2826beac3a677fda3c57
Fluke <42545106+LocalCoinIS@users.noreply.github.com>
2019-11-07 09:48:26 +0200

    Update apiConfig.js

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -3,9 +3,8 @@ export const settingsAPIs = {
     WS_NODE_LIST: [
         {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
         {url: "wss://chippawa-for-life.com", location: "US Denver by crazy-indain"},
-        {url: "wss://id.localcoin.is", location: "Indonesia by billpresta3101"},
         {url: "wss://de.localcoin.is", location: "Germany"},
-        {url: "wss://node.gate.is", location: "Afica"},
+        {url: "wss://node.gate.is", location: "Africa"},
         {url: "wss://fi.localcoin.is", location: "Finland"},
         {url: "wss://mysupremecoin.com", location: "Italy by the-supreme19"},
         {url: "wss://ru.localcoin.is", location: "Russia"}
