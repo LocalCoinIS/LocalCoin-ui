315c7a3a68e62c14e94a98b15b9c0f599e2eaed1
Fluke <42545106+LocalCoinIS@users.noreply.github.com>
2020-03-02 23:07:24 +0200

    Update apiConfig.js

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -4,8 +4,6 @@ export const settingsAPIs = {
         {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
         {url: "wss://chippawa-for-life.com", location: "US Denver by crazy-indain"},
         {url: "wss://de.localcoin.is", location: "Germany"},
-        {url: "wss://de2.localcoin.is", location: "Germany activenode"},
-        {url: "wss://llcnode.buycoinbot.com", location: "Germany by BuyCoinFast"},
         {url: "wss://node.gate.is", location: "Africa"},
         {url: "wss://fi.localcoin.is", location: "Finland"},
         {url: "wss://ru.localcoin.is", location: "Russia"}
